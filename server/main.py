2d4774f9a1bf2557641b712fa991f2e3438e7633
John Samuels <johnsamuels1218@gmail.com>
2019-07-12 17:42:15 -0400

    Move routes into routes/

--- server/main.py
+++ server/main.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from routes.helloworld import HelloWorld
 
 app = Flask(__name__)
 api = Api(app)
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
 
