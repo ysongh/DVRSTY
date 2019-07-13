ad56c3253498c525dd44ba93c687d0a37ba7e005
You Song Hou <yousonghou@yahoo.com>
2019-07-13 12:56:19 -0400

    add error point

--- react/src/components/DataCard.js
+++ react/src/components/DataCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import LineChart from "./Graph";
+import GraphError from './GraphError';
+
 class DataCard extends React.Component {
   render() {
-    console.log(this.props.data);
+    console.log("error", this.props.errorD);
     return (
             <div className="card light-green" style={{width: '100%'}}>
               <div className="card-content black-text">
                 <p>{this.props.cardName} Data:</p>
                 <LineChart data={this.props.data}/>
-                <a href='/variable' className="waves-effect waves-teal btn-flat">More</a>
+                <p>{this.props.cardName} Error:</p>
+                <GraphError data={this.props.errorD} />
               </div>
             </div>
     );
---
+++ react/src/components/GraphError.js
@@ -0,0 +1,54 @@
+import Chart from "react-apexcharts";
+import React from 'react';
+
+class GraphError extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          type: "line",
+        },
+      },
+      series: [
+        {
+          name: "Sensor-1",
+          data: this.props.data.sensor_1
+        },
+        {
+          name: "Sensor-2",
+          data: this.props.data.sensor_2
+        },
+        {
+          name: "Sensor-3",
+          data: this.props.data.sensor_3
+        },
+        {
+          name: "Sensor-4",
+          data: this.props.data.sensor_4
+        },
+      ]
+    };
+  }
+
+  render() {
+    return (
+        <div className="app">
+          <div className="row">
+            <div className="mixed-chart">
+              <Chart
+                  options={this.state.options}
+                  series={this.state.series}
+                  type="line"
+                  width="600"
+                  height="200"
+              />
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default GraphError;
